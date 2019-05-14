fb3e4d14ff88954e39be24a980134fd4427ed62e
river-ceanne <snvencer@gmail.com>
2019-05-13 19:08:45 -0700

    header component mods..

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Nav from './nav.js';
 
 const Header = props => {
   return (
     <header>
       <h1>Market App</h1>
       <p>This is Team None</p>
+      <Nav />
     </header>
   );
 };

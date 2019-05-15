95f788762c96a7d43e219b3ba65b665d97f6e802
Luke Chandler <lhchandler4@gmail.com>
2019-05-15 13:26:59 -0700

    modified portfolio.js with updated info

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -18,7 +18,7 @@ export default class Portfolio extends React.Component{
       <React.Fragment>
         <h2 id="portfolio-h2">My Portfolio</h2>
         <section id="portfolio">
-          <p>This is your portfolio. Welcome back. I hope today your stocks were up.</p>
+          <p>Welcome back to your portfolio. I hope all of your stocks were up today and that the bulls are beating the bears. Search for stocks of interest and add or delete from your portfolio.</p>
           <ul>Placeholder - portfolio name 
           {portfolio}
           </ul>

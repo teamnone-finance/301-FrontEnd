0255983427ffbc25a4e4a1b87efde2537daf09e4
river-ceanne <snvencer@gmail.com>
2019-05-15 20:18:38 -0700

    saving .. before switch branch

--- src/components/nav.js
+++ src/components/nav.js
@@ -25,7 +25,7 @@ export default class Nav extends React.Component {
     navArr.push(<li key = {1}><a href={'/main'}>Home</a></li>);
     if (loggedIn || localLoggedIn){
       navArr.push(<li key = {2}><a href={'/portfolio'}>My Portfolio</a></li>);
-      navArr.push(<li id='logout' key = {4}><a href='/' onClick={this.logout}>Log Out</a></li>);
+      navArr.push(<li id='logout' key = {5}><a href='/' onClick={this.logout}>Log Out</a></li>);
     } else{
       navArr.push(<li key = {2}><a href={'/login'}>Log In</a></li>);
     }

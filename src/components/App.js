963efdb0af14aa7f07e1f8b6908455f6dbcc6384
river-ceanne <snvencer@gmail.com>
2019-05-15 20:20:32 -0700

    <Card /> - took out from <App/> - cause of crash

--- src/components/App.js
+++ src/components/App.js
@@ -62,7 +62,6 @@ class App extends React.Component {
       <>
       <Header loggedIn={this.state.user.loggedIn} handleLogin={this.handleLoggedStatus}
             updateState={this.setStateData}/>
-      <Card />
       <Footer />
       </>
     );
